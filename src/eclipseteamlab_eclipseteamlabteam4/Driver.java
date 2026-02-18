18fde601898148ccb0d2a9b04ecf6c4de3cfbd92
Minh Pham <pqm3502@gmail.com>
2026-02-18 16:30:00 -0500

    We are hungry

--- src/eclipseteamlab_eclipseteamlabteam4/Driver.java
+++ src/eclipseteamlab_eclipseteamlabteam4/Driver.java
@@ -1,7 +1,8 @@
 package eclipseteamlab_eclipseteamlabteam4;
 
 public class Driver {
-public static void main() {
+public static void main(String[] args) {
 	System.out.println("Our team is getting started.");
+	Hungry.hungry();
 }
 }
