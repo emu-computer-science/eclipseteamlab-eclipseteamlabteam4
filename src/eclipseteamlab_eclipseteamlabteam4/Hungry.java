0dc9006c518beb161fcf5c4e4acceb3a3eaaeb6a
Minh Pham <pqm3502@gmail.com>
2026-02-18 17:05:11 -0500

    hello we are celebrating together

--- src/eclipseteamlab_eclipseteamlabteam4/Hungry.java
+++ src/eclipseteamlab_eclipseteamlabteam4/Hungry.java
@@ -3,4 +3,7 @@ public class Hungry {
 	public static void hungry() {
 		System.out.println ("We are hungry");
 	}
+	public static void celebrate() {
+		System.out.println ("We are celebrating together!!!:))))");
+	}
 }
