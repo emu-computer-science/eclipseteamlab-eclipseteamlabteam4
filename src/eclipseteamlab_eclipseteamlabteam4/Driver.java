97ce3ccef8f07fba8ed59e04f585a937804ff108
Minh Pham <pqm3502@gmail.com>
2026-02-18 16:46:44 -0500

    We are all hungry

---
+++ src/eclipseteamlab_eclipseteamlabteam4/Driver.java
@@ -0,0 +1,16 @@
+package eclipseteamlab_eclipseteamlabteam4;
+
+public class Driver {
+<<<<<<< HEAD
+public static void main(String[] args) {
+	System.out.println("Our team is getting started.");
+	Hungry.hungry();
+}
+=======
+	public static void main() {
+		System.out.println("Our team is getting started.");
+		Glad.glad();
+		Sad.sad();
+	}
+>>>>>>> ec0bc1a65c50beead8e0a53f3279627a7098747f
+}
---
+++ src/eclipseteamlab_eclipseteamlabteam4/Glad.java
@@ -0,0 +1,10 @@
+package eclipseteamlab_eclipseteamlabteam4;
+
+public class Glad {
+
+	public static void glad() {
+		System.out.println("We are feeling glad");
+
+	}
+
+}
